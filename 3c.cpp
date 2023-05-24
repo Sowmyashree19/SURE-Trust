800899b331dcb97c6bd5d3ed03a5bd381b5f73f4
sowmya shree N <132283925+Sowmyashree19@users.noreply.github.com>
2023-05-24 21:52:21 +0530

    Add files via upload

---
+++ 3c.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a = 30;
+	int b = 40;
+	int c = 50;
+	int d = (a+b);
+	printf("a is %d,b is %d,c is %d ,d is %d which are two digits ",a,b,c,d);
+	
+	return 0;
+}
